d6a17c99df687627c0a42c06036eb716dc2ddce9
= <mayankrathore9897@gmail.com>
2025-02-23 22:17:22 +0530

    adjusting the layout of file

--- add_admin.py
+++
@@ -1,8 +0,0 @@
-from flask import request
-
-@app.before_request
-def check_jwt():
-    if request.endpoint in ["admin_dashboard"]:  # Secure admin route
-        auth_header = request.headers.get("Authorization")
-        if not auth_header or not auth_header.startswith("Bearer "):
-            return jsonify({"error": "Missing or invalid token"}), 401
